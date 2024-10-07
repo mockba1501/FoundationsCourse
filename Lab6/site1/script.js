77167750b905e006d9a89349d17292f010c1fffb
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2024-10-07 19:55:56 +0300

    Simple Javascript Testing

---
+++ Lab6/site1/script.js
@@ -0,0 +1,15 @@
+//let answer = prompt("What is your name")
+//console.log(answer)
+//document.write("My Name is Mohammed")
+
+//let answer;
+//console.log(answer)
+
+
+let date = new Date();
+console.log(date.toString());
+console.log(date.getDay());
+
+let x = 12, y = 5;
+let result = x / y;
+console.log(result);
